936f1f29688cf3664cd61d3c6675be49b4c799db
Aman Bokdia <amanbokdia@gmail.com>
2021-07-05 12:53:59 +0530

    first program

---
+++ first.java
@@ -0,0 +1,18 @@
+public static class first
+{
+	public void add(int x,int y)
+	{
+		int sum=x+y;
+		System.out.println("Sum=",sum);
+	}
+	public void difference(int x,int y)
+	{
+		int diff=x-y;
+		System.out.println("diff=",diff);
+	}
+	public static void main()
+	{
+		sum(5,5);
+		diff(5,3);
+	}
+}
